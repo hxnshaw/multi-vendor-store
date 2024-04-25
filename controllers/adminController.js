d0d20fb52f0224c2c6b68ad4e7bcb1e390128b84
hxnshaw <eghosasere07@gmail.com>
2024-04-25 15:12:29 +0100

    Admin Controller & Router

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -44,3 +44,39 @@ exports.logoutAdmin = async (req, res) => {
   });
   res.status(StatusCodes.OK).json({ message: "Logged Out" });
 };
+
+exports.getAllAdmins = async (req, res) => {
+  const admins = await Admin.find({}).select("-password");
+  if (admins === null) throw new CustomError.NotFoundError("Admins not found");
+  res.status(StatusCodes.OK).json({ data: admins });
+};
+
+exports.viewMyAdminProfile = async (req, res) => {
+  const adminId = req.user.userId;
+  const admin = await Admin.findOne({ _id: adminId });
+  if (!admin) throw new CustomError.NotFoundError("Admin not found");
+  const tokenUser = createTokenUser(admin);
+  res.status(StatusCodes.OK).json({ data: tokenUser });
+};
+
+exports.getSingleAdmin = async (req, res) => {
+  const { id: adminId } = req.params;
+
+  const admin = await Admin.findOne({ _id: adminId });
+  if (!admin) throw new CustomError.NotFoundError("Admin not found");
+  const tokenUser = createTokenUser(admin);
+  res.status(StatusCodes.OK).json({ data: tokenUser });
+};
+
+exports.updateAdminPassword = async (req, res) => {
+  const { currentPassword, newPassword } = req.body;
+
+  if (!currentPassword || !newPassword)
+    throw new CustomError.BadRequestError("Incomplete credentials");
+  const admin = await Admin.findOne({ _id: req.user.userId });
+  if (!admin)
+    throw new CustomError.NotFoundError("Admin not found, who goes you?");
+  admin.password = newPassword;
+  await admin.save();
+  res.status(StatusCodes.OK).json({ message: "New Password Set" });
+};
--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
+const {
   registerAdmin,
   loginAdmin,
   logoutAdmin,
+  getSingleAdmin,
+  getAllAdmins,
+  viewMyAdminProfile,
+  updateAdminPassword,
 } = require("../controllers/adminController");
 
 router.post("/admins/signup", registerAdmin);
@@ -11,5 +19,25 @@ router.post("/admins/signup", registerAdmin);
 router.post("/admins/login", loginAdmin);
 
 router.post("/admins/logout", logoutAdmin);
+
+//view admin profile
+router
+  .route("/admins/my-admin-dashboard")
+  .get(authenticateUser, authorizePermissions("admin"), viewMyAdminProfile);
+
+//update admin password
+router
+  .route("/admins/my-admin-dashboard/update-password")
+  .put(authenticateUser, authorizePermissions("admin"), updateAdminPassword);
+
+//view all admins
+router
+  .route("/admins/view-all-admins")
+  .get(authenticateUser, authorizePermissions("admin"), getAllAdmins);
+
+//view each admin
+router
+  .route("/admins/:id")
+  .get(authenticateUser, authorizePermissions("admin"), getSingleAdmin);
 
 module.exports = router;
