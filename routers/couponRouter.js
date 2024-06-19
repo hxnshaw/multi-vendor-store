056407ecf7d2d694d82d4018dbf123ad29b8cecb
hxnshaw <eghosasere07@gmail.com>
2024-06-19 16:37:35 +0100

    comments in coupon routers

--- routers/couponRouter.js
+++ routers/couponRouter.js
@@ -10,14 +10,17 @@ const {
   authorizePermissions,
 } = require("../middleware/authentication");
 
+//route for vendors to create coupon codes
 router
   .route("/coupons/create-coupon-code")
   .post(authenticateUser, authorizePermissions("vendor"), createCoupon);
 
+  //update coupon codes route
 router
   .route("/coupons/update-coupon/:couponId")
   .put(authenticateUser, authorizePermissions("vendor"), updateCouponCode);
 
+  //vendor route to delete coupon codes
 router
   .route("/coupons/delete-coupon/:couponId")
   .delete(authenticateUser, authorizePermissions("vendor"), deleteCouponCode);
